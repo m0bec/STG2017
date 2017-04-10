3c759134f7d8b3a2b24ed3438457cc79554590b0
mobec <mobec622@outlook.jp>
2017-04-10 10:19:45 +0900

    画面を作成

---
+++ STG2017Pro/main.cpp
@@ -0,0 +1,45 @@
+#include "DxLib.h"
+#include <math.h>
+
+// WinMain関数
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードの設定
+	ChangeWindowMode(true);
+	SetGraphMode(640, 480, 16);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1) return -1;
+
+	// グラフィックの描画先を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+
+	// 移動ルーチン
+	while (1)
+	{
+		// 画面を初期化(真っ黒にする)
+		ClearDrawScreen();
+
+		
+
+		
+
+		// 裏画面の内容を表画面にコピーする
+		ScreenFlip();
+
+		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
+		// -1 が返ってきたらループを抜ける
+		if (ProcessMessage() < 0) break;
+
+		// もしＥＳＣキーが押されていたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+	}
+
+	// ＤＸライブラリ使用の終了処理
+	DxLib_End();
+
+	// ソフトの終了
+	return 0;
+}
