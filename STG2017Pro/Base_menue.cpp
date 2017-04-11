4c8edea0969a21d5a4a916e2304043a73e67ecae
mobec <mobec622@outlook.jp>
2017-04-11 15:32:37 +0900

    menueのスーパークラスが完成

---
+++ STG2017Pro/Base_menue.cpp
@@ -0,0 +1,42 @@
+#include "Base_menue.h"
+
+Base_menue::Base_menue(int menue_number_, int &select_number_, int &this_graph_, int x_, int y_)
+{
+	menue_number = menue_number_;
+	pselect_number = &select_number_;
+	this_graph = &this_graph_;
+	x = x_;
+	y = y_;
+	exit_flag = false;
+}
+
+void Base_menue::dispThis()
+{
+	if (*pselect_number == menue_number) {
+		DrawGraph(x, y, *this_graph, true);
+	}
+	else {
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);
+		DrawGraph(x, y, *this_graph, true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+}
+
+void Base_menue::Exit()
+{
+	if (exit_flag) {
+		*pselect_number = 0;
+		exit_flag = false;
+	}
+}
+
+void Base_menue::setExitflag()
+{
+	exit_flag = true;
+}
+
+void Base_menue::Run()
+{
+	dispThis();
+	Exit();
+}
--- STG2017Pro/Base_menue.h
+++ STG2017Pro/Base_menue.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "DxLib.h"
+
+class Base_menue
+{
+private:
+	void dispThis();
+	void Exit();
+
+	int menue_number;
+	int *pselect_number;
+	int *this_graph;
+	int x;
+	int y;
+	bool exit_flag;
+
+public:
+	Base_menue(int menue_number_, int &select_number_, int &this_graph_, int x_, int y_);
+	void setExitflag();
+	void Run();
+};
